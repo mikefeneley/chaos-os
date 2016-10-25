ba1e11571a5da4d8df0be82c85c46cf603c7a5ea
Michael <michael.j.feneley@gmail.com>
2016-10-25 16:43:36 -0400

    First kernel code callded

--- src/boot_sec.asm
+++ src/boot_sec.asm
@@ -1,6 +1,9 @@
 
 
 [org 0x7c00] 
+KERNEL_OFFSET equ 0x1000
+
+    mov [BOOT_DRIVE], dl
 
     mov bp, 0x9000
 
@@ -8,30 +11,50 @@
    
     mov bx, MSG_REAL_MODE
     call print_string
-   
+  
+    call load_kernel
+    
     call switch_to_pm ; Note that we never return from here.
     jmp $
 
 
 
-%include "test.asm"
+;%include "test.asm"
 ;%include "print_reg.asm"
-%include "print_hex.asm"
+;%include "print_hex.asm"
 %include "print_string.asm" ; Re - use our print_string function
 %include "disk_load.asm"
 %include "print.asm"
 %include "gdt.asm"
 %include "switch.asm"
 
+[bits 16]
+
+load_kernel :
+    mov bx, MSG_LOAD_KERNEL ; Print a message to say we are loading the kernel
+    call print_string
+    mov bx, KERNEL_OFFSET ; Set -up parameters for our disk_load routine , so
+    mov dh, 15 ; that we load the first 15 sectors ( excluding
+    mov dl, [BOOT_DRIVE] ; the boot sector ) from the boot disk ( i.e. our
+    call disk_load ; kernel code ) to address KERNEL_OFFSET
+    ret
+
+[bits 32]
+
 BEGIN_PM:
     mov ebx, MSG_PROT_MODE
     call print_string_pm ; Use our 32 - bit print routine.
+   
+    call KERNEL_OFFSET
     jmp $ ; Hang.
 
 
-MSG_REAL_MODE db "Started in 16 bit real mode", 0
-MSG_PROT_MODE: 
-    db "Switched to 32 bit mode", 0
+BOOT_DRIVE db 0
+MSG_REAL_MODE db " Started in 16 - bit Real Mode " , 0
+MSG_PROT_MODE db " Successfully landed in 32 - bit Protected Mode " , 0
+MSG_LOAD_KERNEL db " Loading kernel into memory. " , 0
+
+
 
 ; Bootsector padding
 times 510-($-$$) db 0
