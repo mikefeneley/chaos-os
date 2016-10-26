e3dea715fcb19f9232354d49b6c55984d2692c31
Michael <michael.j.feneley@gmail.com>
2016-10-26 06:57:59 -0400

    add missing

---
+++ src/boot/disk_load.asm
@@ -0,0 +1,52 @@
+
+
+;mov bx,HELLO_WORLD 
+;call print_string
+;mov bx, GOODBYE_WORLD
+;call print_string
+
+;jmp disk_load
+
+; load DH sectors to ES:BX from drive DL
+disk_load :
+    push dx ; Store DX on stack so later we can recall
+
+    ; how many sectors were request to be read ,
+    ; even if it is altered in the meantime
+    mov ah, 0x02 ; BIOS read sector function
+    mov al, dh ; Read DH sectors
+    mov dh, 0x00 ; Select head 0
+
+
+    mov ch, 0x00
+    mov cl, 0x02 ; Start reading from second sector ( i.e.
+
+;    call print_reg
+
+
+    ; after the boot sector )
+    int 0x13 ; BIOS interrupt
+
+;    call print_reg
+   
+;    jc disk_error ; Jump if error ( i.e. carry flag set )
+
+    pop dx ; Restore DX from the stack
+    cmp dh , al ; if AL ( sectors read ) != DH ( sectors expected )
+;    jne disk_error ; display error message
+    ret
+
+disk_error :
+    mov bx, DISK_ERROR_MSG
+    call print_string
+    jmp $
+
+; Variables
+DISK_ERROR_MSG:
+    db "Disk read error", 0
+
+HELLO_WORLD:
+    db "HelloWorldz", 0
+
+GOODBYE_WORLD:
+    db "Goodbye Worldz", 0
